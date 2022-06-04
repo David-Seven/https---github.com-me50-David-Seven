8a8ebe907b6950c944361b62d75da94aa81eebef
David-Seven <106294746+David-Seven@users.noreply.github.com>
2022-06-04 09:07:16 -0700

    Add files via upload

---
+++ indoor/indoor.py
@@ -0,0 +1,11 @@
+#This program will take an input string from the user
+#It will conver the string to all lower case
+#It will preserve whitespace and punctuation
+
+#string function used is str.lower()
+
+def main():
+    phrase = input("Type in a string of charaters: ")
+    print(phrase.lower())
+
+main()
